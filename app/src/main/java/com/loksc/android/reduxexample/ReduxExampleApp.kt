6793d225e388c34949056450d7b9365debedf0cc
gg1140 <gordonlschan@gmail.com>
2022-01-04 17:06:05 +0800

    add Store to Application

--- app/src/main/java/com/loksc/android/reduxexample/ReduxExampleApp.kt
+++ app/src/main/java/com/loksc/android/reduxexample/ReduxExampleApp.kt
@@ -1,5 +1,8 @@
 package com.loksc.android.reduxexample
 
 import android.app.Application
+import com.loksc.android.reduxexample.redux.Redux
 
-class ReduxExampleApp: Application() {}
+class ReduxExampleApp: Application() {
+    val store = Redux.store
+}
